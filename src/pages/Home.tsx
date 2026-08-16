de99edd58873940ffc47353a53b7716b87ca5bd2
fearyfiremast <fearyfiremast@gmail.com>
2026-08-15 19:26:08 -0700

    Feat: Added structure of homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,18 @@ import PageTemplate from "../PageTemplate"
 const Home = () => {
   return (
     <PageTemplate>
-      <p>Hello</p>
+      <div>
+        <h1>Xander Smith</h1>
+        <p>
+          I am a 4th year computer science student at Simon Fraser University and junior developer at Blueprint. I'm passionate about designing easy to extend systems and interactive and vibrant interfaces using JavaScript and CSS. I care deeply about the utility and quality of my projects.
+        </p>
+      </div>
+      <hr/>
+      <div>
+        <h2>
+          Showcase
+        </h2>
+      </div>
     </PageTemplate>
   )
 }
