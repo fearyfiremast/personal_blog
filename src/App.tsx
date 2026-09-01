e1de9d86704ab78a5fac71915ec85f743f6bb7e7
fearyfiremast <fearyfiremast@gmail.com>
2026-09-01 14:31:07 -0700

    Feat: Added appropriate app wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,20 @@ import {  Routes, Route  } from "react-router"
 
 import Home from "./pages/Home"
 import NavBar from "./components/NavBar"
+import Footer from "./components/Footer"
 
 function App() {
 
   return (
-    <>
-    <NavBar/>
-    <Routes>
-      <Route path="/" element={<Home />}/>
-    </Routes>
-    </>
+    <div className="flex flex-col h-[min(100vh)]">
+      <NavBar/>
+      <main className="flex grow">
+        <Routes>
+          <Route path="/" element={<Home />}/>
+        </Routes>
+      </main>
+      <Footer/>
+    </div>
   )
 }
 
